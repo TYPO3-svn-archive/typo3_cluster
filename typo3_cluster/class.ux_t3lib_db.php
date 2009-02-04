b198de66e1658fdc7f284b758ecae8e1c2aec806
echo2k <echo2k@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-04 19:16:31 +0000

    Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/typo3_cluster/trunk@16449 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3_cluster/class.ux_t3lib_db.php
+++ typo3_cluster/class.ux_t3lib_db.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2004-2008 Kasper Skaarhoj (kasperYYYY@typo3.com)
+*  (c) 2008 Saverio Vigni <s.vigni@hor-net.com>, Federico Stendardi <stenda85@alice.it>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
